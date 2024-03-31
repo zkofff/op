c806a0d03ac22fa86d2f64f9cd26788943c9b6bf
zkofff <tupaarbuz@gmail.com>
2024-03-31 21:54:23 +0300

    append push / pop functions

--- libs/data_structures/vector/vectorVoid.c
+++ libs/data_structures/vector/vectorVoid.c
@@ -40,3 +40,36 @@ void deleteVectorV(vectorVoid *v) {
     v->baseTypeSize = 0;
     free(v->data);
 }
+
+bool isEmptyV(vectorVoid *v) {
+    return v->size == 0 ? true : false;
+}
+bool isFullV(vectorVoid *v) {
+    return v->size == v->capacity ? true : false;
+}
+void getVectorValueV(vectorVoid *v, size_t index, void *destination) {
+    char *source = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+}
+void setVectorValueV(vectorVoid *v, size_t index, void *source) {
+    char *destination = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+}
+
+void popBackV(vectorVoid *v) {
+    if (isEmptyV(v)) {
+        fprintf(stderr, "vector void is empty");
+        exit(1);
+    }
+    (v->size)--;
+}
+void pushBackV(vectorVoid *v, void *source) {
+    if (v->size == v->capacity) {
+        size_t newCapacity = v->capacity*2;
+        if (v->capacity == 0) {
+            newCapacity = 1;
+        }
+        reserveV(v, newCapacity);
+    }
+    setVectorValueV(v, v->size++, source);
+}
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-void test() {
-    test_pushBack_emptyVector();
-    test_pushBack_fullVector();
-    test_popBack_notEmptyVector();
-    test_atVector_notEmptyVector();
-    test_atVector_requestToLastElement();
-    test_back_oneElementInVector();
-    test_front_oneElementInVector();
-// последующие тесты
-}
+int main() {
+    size_t n;
+    scanf("%zd", &n);
+    vectorVoid v = createVectorV(0, sizeof(float));
+    for (int i = 0; i < n; i++) {
+        float x;
+        scanf("%f", &x);
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        printf("%f ", x);
+    }
