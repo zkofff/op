7ffaba7ce284358769d073b8396373b95298c3b4
zkofff <tupaarbuz@gmail.com>
2024-03-31 22:01:34 +0300

    refactoring / bug fix

--- libs/data_structures/vector/vectorVoid.c
+++ libs/data_structures/vector/vectorVoid.c
@@ -73,3 +73,34 @@ void pushBackV(vectorVoid *v, void *source) {
     }
     setVectorValueV(v, v->size++, source);
 }
+
+void test1() {
+    size_t n;
+    scanf("%zd", &n);
+    vectorVoid v = createVectorV(0, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        int x;
+        getVectorValueV(&v, i, &x);
+        printf("%d ", x);
+    }
+}
+void test2() {
+    size_t n;
+    scanf("%zd", &n);
+    vectorVoid v = createVectorV(0, sizeof(float));
+    for (int i = 0; i < n; i++) {
+        float x;
+        scanf("%f", &x);
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        printf("%f ", x);
+    }
+}
