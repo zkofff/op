bcfd0425c464ce9bad1d391e74a8647469441db3
zkofff <tupaarbuz@gmail.com>
2024-03-31 23:34:50 +0300

    edits + testing

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "libs/data_structures/bitset/bitset.h"
+#include "libs/data_structures/vector/vector.h"
+#include "libs/data_structures/vector/vectorVoid.h"
+#include "libs/data_structures/matrix/matrix.h"
+
 int main() {
-    size_t n;
-    scanf("%zd", &n);
-    vectorVoid v = createVectorV(0, sizeof(float));
-    for (int i = 0; i < n; i++) {
-        float x;
-        scanf("%f", &x);
-        pushBackV(&v, &x);
-    }
-    for (int i = 0; i < n; i++) {
-        float x;
-        getVectorValueV(&v, i, &x);
-        printf("%f ", x);
-    }
+    tests();
+    return 0;
+}
