15663c643de770e919258e941a0bb5a7d24c3a0d
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:26:53 +0300

    task 10 + testing

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -842,6 +842,48 @@ void test_sortByDistances() {
     freeMemMatrix(&result);
 }
 
+int countNUnique(int *a, int n) {
+    int *unique_numbers = malloc(sizeof(int)*n);
+    int len = 0;
+    for (int i = 0; i < n; i++) {
+        bool is_in = false;
+        for (int j = 0; j < len && !is_in; j++) {
+            if (a[i] == unique_numbers[j]) {
+                is_in = true;
+            }
+        }
+        if (!is_in) {
+            unique_numbers[len++] = a[i];
+        }
+    }
+    free(unique_numbers);
+    return len;
+}
+int countEqClassesByRowsSum(matrix m) {
+    int *unique_sums = malloc(sizeof(int)*m.nRows);
+    for (int i = 0; i < m.nRows; i++) {
+        unique_sums[i] = getSum(m.values[i], m.nCols);
+    }
+    int result = countNUnique(unique_sums, m.nRows);
+    free(unique_sums);
+    return result;
+}
+void test_countEqClassesByRowsSum_1() {
+    matrix m = createMatrixFromArray((int[]) {},0, 0);
+    assert(countEqClassesByRowsSum(m) == 0);
+    freeMemMatrix(&m);
+}
+void test_countEqClassesByRowsSum_2() {
+    matrix m = createMatrixFromArray((int[]) {7, 1,
+                                              2, 7,
+                                              5, 4,
+                                              4, 3,
+                                              1, 6,
+                                              8, 0},6, 2);
+    assert(countEqClassesByRowsSum(m) == 3);
+    freeMemMatrix(&m);
+}
+
 
 
 
