289899351a2c603dcd00c814c0766e5b1d0f1d6c
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:40:26 +0300

    task 17 + testing

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1157,5 +1157,40 @@ void test_getNSpecialElement2() {
     freeMemMatrix(&m);
 }
 
+double getScalarProduct(int *a, int *b, int n) {
+    double result = 0;
+    for (int i = 0; i < n; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+double getCosine(int *a, int *b, int n) {
+    float len_a = getDistance(a, n);
+    float len_b = getDistance(b, n);
+    if (len_a == 0 || len_b == 0) {
+        fprintf(stderr, "zeros vector`s length");
+    }
+    return getScalarProduct(a, b, n) / (len_a * len_b);
+}
+int getVectorIndexWithMaxAngle(matrix m, int *b) {
+    int result = 0;
+    double max_cos = getCosine(m.values[0], b, m.nCols);
+    for (int i = 1; i < m.nRows; i++) {
+        double cos = getCosine(m.values[i], b, m.nCols);
+        if (cos > max_cos) {
+            max_cos = cos;
+            result = i;
+        }
+    }
+    return result;
+}
+void test_getVectorIndexWithMaxAngle() {
+    matrix m = createMatrixFromArray((int[]) {1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3,3);
+    int b[] = {5, 8, 9};
+    assert(getVectorIndexWithMaxAngle(m, b) == 1);
+    freeMemMatrix(&m);
+}
 
 
