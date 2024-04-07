59ff6601d733779b41c432e9922e5ccb7dfff1f3
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:12:42 +0300

    task 4

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -513,5 +513,27 @@ void sortColsByMinElement(matrix *m) {
     }
 }
 
+matrix mulMatrices(matrix m1, matrix m2) {
+    int rows = m1.nRows;
+    int cols = m1.nCols;
+    matrix result = getMemMatrix(rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int sum = 0;
+            for (int g = 0; g < rows; g++) {
+                sum += m1.values[i][g] * m2.values[g][j];
+            }
+            result.values[i][j] = sum;
+        }
+    }
+    return result;
+}
+void getSquareOfMatrixIfSymmetric(matrix *m) {
+    if (isSymmetricMatrix(m)) {
+        *m = mulMatrices(*m, *m);
+    }
+}
+
+
 
 
