bcd9b362e1f31f972872a23180b874eedd4c77ca
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:28:06 +0300

    task 11 + testing

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -884,6 +884,33 @@ void test_countEqClassesByRowsSum_2() {
     freeMemMatrix(&m);
 }
 
+int getNSpecialElement(matrix m) {
+    int result = 0;
+    for (int i = 0; i < m.nCols; i++) {
+        int sum = 0;
+        for (int j = 0; j < m.nRows; j++) {
+            sum += m.values[j][i];
+        }
+        for (int j = 0; j < m.nRows; j++) {
+            if (m.values[j][i] > sum - m.values[j][i]) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+void test_getNSpecialElement_1() {
+    matrix m = createMatrixFromArray((int[]) {},0, 0);
+    assert(getNSpecialElement(m) == 0);
+    freeMemMatrix(&m);
+}
+void test_getNSpecialElement_2() {
+    matrix m = createMatrixFromArray((int[]) {3, 5,5, 4,
+                                              2, 3, 6, 7,
+                                              12, 2, 1, 2},3, 4);
+    assert(getNSpecialElement(m) == 2);
+    freeMemMatrix(&m);
+}
 
 
 
