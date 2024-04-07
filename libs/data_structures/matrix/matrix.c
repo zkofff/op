56af39c09a7073caa832bab101a62e4b733c76e1
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:31:18 +0300

    task 13 + testing

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -943,5 +943,66 @@ void test_swapPenultimateRow_2() {
     freeMemMatrix(&result);
 }
 
+bool isNonDescendingSorted(int *a, int n) {
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+bool hasAllNonDescendingRows(matrix m) {
+    for (int i = 0; i < m.nRows; i++) {
+        if (!isNonDescendingSorted(m.values[i], m.nCols)) {
+            return false;
+        }
+    }
+    return true;
+}
+int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
+    int result = 0;
+    for (int i = 0; i < nMatrix; i++) {
+        if (hasAllNonDescendingRows(ms[i])) {
+            result++;
+        }
+    }
+    return result;
+}
+void test_countNonDescendingRowsMatrices_1() {
+    matrix m1 = createMatrixFromArray((int[]) {7, 1,
+                                               1, 1},2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 6,
+                                               2, 2},2, 2);
+    matrix m3 = createMatrixFromArray((int[]) {5, 4,
+                                               2, 3},2, 2);
+    matrix m4 = createMatrixFromArray((int[]) {1, 3,
+                                               7, 9},2, 2);
+    matrix *ms = getMemArrayOfMatrices(4, 2, 2);
+    ms[0] = m1;
+    ms[1] = m2;
+    ms[2] = m3;
+    ms[3] = m4;
+    assert(countNonDescendingRowsMatrices(ms, 4) == 2);
+    freeMemMatrices(ms, 4);
+}
+void test_countNonDescendingRowsMatrices_2() {
+    matrix m1 = createMatrixFromArray((int[]) {7, 1,
+                                               1, 1},2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 0,
+                                               2, 2},2, 2);
+    matrix m3 = createMatrixFromArray((int[]) {5, 4,
+                                               2, 3},2, 2);
+    matrix m4 = createMatrixFromArray((int[]) {10, 3,
+                                               7, 9},2, 2);
+    matrix *ms = getMemArrayOfMatrices(4, 2, 2);
+    ms[0] = m1;
+    ms[1] = m2;
+    ms[2] = m3;
+    ms[3] = m4;
+    assert(countNonDescendingRowsMatrices(ms, 4) == 0);
+    freeMemMatrices(ms, 4);
+}
+
+
 
 
