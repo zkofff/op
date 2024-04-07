0459242c296a41992c0f3f6d1fe969adc5cbd672
zkofff <tupaarbuz@gmail.com>
2024-04-07 21:37:04 +0300

    task 15 + testing

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1068,5 +1068,61 @@ void test_printMatrixWithMaxZeroRows() {
     freeMemMatrices(ms, 5);
 }
 
+int getMatrixNorm(matrix m) {
+    int res = 0;
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            int norm = abs(m.values[i][j]);
+            if (norm > res) {
+                res = norm;
+            }
+        }
+    }
+    return res;
+}
+
+void printMatricesWithMinNorm(matrix *ms, int n) {
+    if (n > 0) {
+        int *norms = malloc(sizeof(int)*n);
+        norms[0] = get_matrix_norm(ms[0]);
+        int min_norm = norms[0];
+        for (int i = 1; i < n; i++) {
+            norms[i] = get_matrix_norm(ms[i]);
+            if (norms[i] < min_norm) {
+                min_norm = norms[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (norms[i] == min_norm) {
+                outputMatrix(ms[i]);
+            }
+        }
+    }
+}
+void test_printMatricesWithMinNorm_1() {
+    matrix *ms = getMemArrayOfMatrices(0, 0, 0);
+    printMatricesWithMinNorm(ms, 0);
+}
+void test_printMatricesWithMinNorm_2() {
+    matrix m1 = createMatrixFromArray((int[]) {2, 1,
+                                               1, 3},2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 1,
+                                               2, 1},2, 2);
+    matrix m3 = createMatrixFromArray((int[]) {3, 4,
+                                               1, 5},2, 2);
+    matrix m4 = createMatrixFromArray((int[]) {4, 4,
+                                               4, 1},2, 2);
+    matrix m5 = createMatrixFromArray((int[]) {2, 1,
+                                               1, 2},2, 2);
+    matrix *ms = getMemArrayOfMatrices(5, 3, 2);
+    ms[0] = m1;
+    ms[1] = m2;
+    ms[2] = m3;
+    ms[3] = m4;
+    ms[4] = m5;
+    printMatricesWithMinNorm(ms, 5);
+    freeMemMatrices(ms, 5);
+}
+
 
 
