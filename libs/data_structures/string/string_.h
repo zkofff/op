c08032f1c8d70435105d26513eb37ba77ba7fcd9
zkofff <tupaarbuz@gmail.com>
2024-04-21 14:55:18 +0300

    search

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -4,5 +4,14 @@
 
 size_t strlen_(const char *begin);
 
+char* find(char *begin, char *end, int ch);
+
+char* findNonSpace(char *begin);
+
+char* findSpace(char *begin);
+
+char* findNonSpaceReverse(char *rbegin, const char *rend);
+
+char* findSpaceReverse(char *rbegin, const char *rend);
 
 #endif
